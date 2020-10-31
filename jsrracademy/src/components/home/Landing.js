2622e622c9a2a911244ecf5b9556de96a0f72b84
raksroxy <rakshithshetty962@gmail.com>
2020-10-31 19:43:35 +0530

    updated now

--- jsrracademy/src/components/home/Landing.js
+++ jsrracademy/src/components/home/Landing.js
@@ -1,15 +1,22 @@
 import React from "react";
 import Home from "./Home";
+import "./Home.css";
+
 const Landing = () => {
   return (
     <>
       <div className="landing">
-        <div className="parti">
-          <Home />
-        </div>
-        <div className="main-content">
-          <p>Hello every one</p>
-        </div>
+        <Home />
+      </div>
+      <div className="land-cont">
+        <p class="items-1">
+          I WILL PRAISE YOU LORD, WITH ALL MY HEART, I WILL TELL OF ALL THE
+          MARVELOUS THINGS YOU HAVE DONE.
+        </p>
+
+        <p class="items-2">I WILL GIVE THANKS TO YOU, WITH ALL MY HEART.</p>
+
+        <p class="items-3">YOUR UNFAILING LOVE IS BETTER THAN LIFE ITSELF.</p>
       </div>
     </>
   );
