323b595a7f19ba205b6f0bd7c8a46f91bf66cf27
mlmtt1998 <mattmell98@gmail.com>
2020-11-02 15:29:13 +0530

    Deployment

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURL: process.env.MONGO_URI,
+  mongoURI: process.env.MONGO_URI,
   secretOrKey: process.env.SECRET_OR_KEY,
   contact_user: process.env.CONTACT_ID,
   contact_password: process.env.CONTACT_PASS,
